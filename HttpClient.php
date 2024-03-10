f3b2ccd2f68bf0a2d505db1f8fd6234f485f6942
virtualynx <eryandi.angga.pratama@gmail.com>
2024-03-10 11:45:33 +0700

    optimize code and add shared_session mechanism

---
+++ HttpClient.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Bbt\Sso;
+
+class HttpClient {
+    private $proxy;
+
+    function __construct(Proxy $proxy = null){
+        $this->proxy = $proxy;
+    }
+
+    public function post($url, $params, $bearer = ''){
+        $curl = curl_init($url);
+        
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_FAILONERROR, false);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 60);
+
+        if(!empty($params)){
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+        }
+
+        if(!empty($bearer)){
+            curl_setopt($curl, CURLOPT_HTTPHEADER, ["Authorization: Bearer $bearer"]);
+        }
+
+        if(!empty($this->proxy)){
+            curl_setopt($curl, CURLOPT_PROXY, $this->proxy->proxy_url);
+            if(!empty($this->proxy->proxy_username)){
+                $proxyUsername = $this->proxy->proxy_username;
+                $proxyPassword = $this->proxy->proxy_password;
+                curl_setopt($curl, CURLOPT_PROXYUSERPWD, "$proxyUsername:$proxyPassword");
+            }
+        }
+        
+        $curlResponse = curl_exec($curl);
+        $http_resp_code = curl_getinfo($curl, CURLINFO_RESPONSE_CODE);
+        
+        $error_no = '';
+        $error_msg = '';
+        if(($error_no = curl_errno($curl))) {
+            $error_msg = curl_error($curl);
+        }
+        curl_close($curl);
+
+        if($http_resp_code >= 400){
+            $msg = !empty($curlResponse)? $curlResponse: $error_msg;
+            throw new \Exception($msg, $http_resp_code);
+        }else if($error_no != 0){
+            throw new \Exception($error_msg, $error_no);
+        }
+
+        return $curlResponse;
+    }
+}
